dbfd589b1e8ac681513a857a93a074c0e1186fa9
Andreea Ghenea <andreeag@Andreeas-MacBook-Pro.local>
2023-07-30 17:58:43 +0300

    Test 2 for checking if it navigates to the Error page

--- tests/ArticleSummaryPage.test.jsx
+++ tests/ArticleSummaryPage.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import ArticleSummaryPage from '../src/pages/ArticleSummaryPage';
+import ErrorPage from '../src/pages/ErrorPage';
 import articleTestData from './articleTestData.json';
 
 describe('Article Summary Page Tests', () => {
@@ -32,5 +33,37 @@ describe('Article Summary Page Tests', () => {
 
         // Assert
         expect(screen.getByText('Test Article 1')).toBeInTheDocument();
+    });
+
+    it('should navigate to the Error Page when an article is not found', () => {
+        // Arrange
+        const invalidArticleId = 'some_invalid_article_id';
+        const routes = [
+            {
+                path: '/error',
+                element: <ErrorPage />,
+            },
+            {
+                path: '/article/:id',
+                element: <ArticleSummaryPage articles={articleTestData} />,
+            },
+        ];
+
+        // Act
+        const router = createMemoryRouter(
+            routes,
+            {
+                initialEntries: [`/article/${invalidArticleId}`],
+                initialIndex: 0,
+            }
+        );
+
+        render(
+            <RouterProvider router={router} />
+        );
+
+        // Assert
+        const errorPageElement = screen.getByText('Oops! A Mischievous Dog Chewed Up This Page');
+        expect(errorPageElement).toBeInTheDocument();
     });
 })
